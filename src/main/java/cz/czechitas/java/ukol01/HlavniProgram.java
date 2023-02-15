f5f9df186b9fc1deac46d3dd80e48a20c0df191c
LucyV25 <lucievoleman@gmail.com>
2023-02-15 20:07:16 +0100

    nakresli prasatko s ocaskem

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -3,13 +3,51 @@ package cz.czechitas.java.ukol01;
 import cz.czechitas.java.ukol01.engine.*;
 
 public class HlavniProgram {
-
+    private Turtle zofka;
     public void start() {
-        Turtle zofka = new Turtle();
+        zofka = new Turtle();
 
-        //TODO implementace domácího úkolu
+        nakresliObdelnik();
+        zofka.turnLeft(45);
+        nakresliHlavu();
+        zofka.turnRight(135);
+        zofka.move(100);
+        nakresliNozicky();
+        zofka.turnRight(135);
+        zofka.move(140);
+        zofka.turnRight(90);
+        nakresliNozicky();
+        zofka.turnRight(45);
+        zofka.move(100);
+        zofka.turnRight(45);
+        zofka.move(30);
     }
 
+    private void nakresliNozicky() {
+        zofka.turnRight(45);
+        zofka.move(30);
+        zofka.turnRight(180);
+        zofka.move(30);
+        zofka.turnRight(90);
+        zofka.move(30);
+        zofka.turnRight(180);
+        zofka.move(30);
+    }
+
+    private void nakresliHlavu() {
+        zofka.move(70);
+        zofka.turnRight(90);
+        zofka.move(70);
+    }
+
+    private void nakresliObdelnik() {
+        for (int i = 0; i < 2; i++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(140);
+            zofka.turnRight(90);
+        }
+    }
     public static void main(String[] args) {
         new HlavniProgram().start();
     }
