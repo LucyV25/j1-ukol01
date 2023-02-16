21a86fd79aaa6bc2b1a3255663ba8646d29c32a6
LucyV25 <lucievoleman@gmail.com>
2023-02-16 21:14:31 +0100

    nakresleni obrazku cast 3

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -7,8 +7,98 @@ public class HlavniProgram {
     public void start() {
         zofka = new Turtle();
 
-        //nakresli prasatko
+//        //nakresli prasatko
+//        nakresliPrasatko();
+//
+//        //nakresli 8uhelnik
+//        zofka.penUp();
+//        zofka.turnLeft(135);
+//        zofka.move(800);
+//        zofka.turnRight(135);
+//        zofka.penDown();
+//        nakresliOsmiUhelnik();
+//
+//        //vychozi bod pro kruh
+//        zofka.turnRight(45);
+//        zofka.penUp();
+//        zofka.move(200);
+//        zofka.turnLeft(90);
+//        zofka.move(40);
+//        zofka.turnRight(90);
+//        zofka.penDown();
+//
+//        //nakresli kruh
+//        nakresliKruh();
+//
+//        //vychozi bod pro slunce
+//        zofka.turnLeft(15);
+//        zofka.penUp();
+//        zofka.move(200);
+//        zofka.penDown();
+
+        //vychozi bod pro finalni obrazek-slunce
         zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(400);
+        zofka.turnRight(90);
+        zofka.move(220);
+        zofka.penDown();
+        //nakresli slunicko
+        nakresliSlunce();
+
+        //nakresli domecky
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(230);
+        zofka.turnLeft(180);
+        zofka.penDown();
+        for (int i = 0; i < 5; i++) {
+            nakresliDomek();
+            posunSeNaDalsiDomek();
+        }
+        zofka.penUp();
+        zofka.turnLeft(180);
+        zofka.move(200);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.penDown();
+        nakresliDomek();
+        zofka.penUp();
+        zofka.turnRight(45);
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(700);
+        zofka.turnRight(90);
+        zofka.penDown();
+        nakresliDomek();
+
+        zofka.penUp();
+        zofka.turnRight(45);
+        zofka.move(100);
+        zofka.turnRight(180);
+        zofka.penDown();
+
+        //nakresli prase
+        zofka.penUp();
+        nakresliPrasatko();
+
+        //vychozi bod psani jmena
+        zofka.turnLeft(45);
+        zofka.penUp();
+        zofka.move(300);
+        zofka.turnLeft(90);
+        zofka.move(250);
+    }
+
+    private void nakresliDomek() {
+        nakresliCtverec();
+        //dostane se na vychozi bod pro kresleni strechy
+        vychoziBodProStrechu();
+        nakresliStrechu();
+    }
+
+    private void nakresliPrasatko() {
         zofka.turnRight(90);
         zofka.move(200);
         zofka.turnLeft(90);
@@ -27,35 +117,6 @@ public class HlavniProgram {
         zofka.move(100);
         zofka.turnRight(45);
         zofka.move(30);
-
-        //nakresli 8uhelnik
-        zofka.penUp();
-        zofka.turnLeft(135);
-        zofka.move(800);
-        zofka.turnRight(135);
-        zofka.penDown();
-        nakresliOsmiUhelnik();
-
-        //vychozi bod pro kruh
-        zofka.turnRight(45);
-        zofka.penUp();
-        zofka.move(200);
-        zofka.turnLeft(90);
-        zofka.move(40);
-        zofka.turnRight(90);
-        zofka.penDown();
-
-        //nakresli kruh
-        nakresliKruh();
-
-        //vychozi bod pro slunce
-        zofka.turnLeft(15);
-        zofka.penUp();
-        zofka.move(200);
-        zofka.penDown();
-
-        //nakresli slunicko
-        nakresliSlunce();
     }
 
     private void nakresliNozicky() {
@@ -104,18 +165,48 @@ public class HlavniProgram {
         for (int i = 0; i < 12; i++) {
             zofka.move(5);
             zofka.turnLeft(90);
-            zofka.move(30);
+            zofka.move(20);
             zofka.turnRight(180);
-            zofka.move(30);
+            zofka.move(20);
             zofka.turnLeft(90);
             zofka.turnRight(5);
             for (int d = 0; d < 5; d++) {
-                zofka.move(5);
+                zofka.move(2);
                 zofka.turnRight(5);
             }
 
 
         }
+    }
+
+    private void nakresliCtverec() {
+        for (int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+        }
+    }
+    private void nakresliStrechu() {
+        zofka.penDown();
+        zofka.turnRight(45);
+        zofka.move(70);
+        zofka.turnRight(90);
+        zofka.move(70);
+    }
+
+    private void vychoziBodProStrechu() {
+        zofka.penUp();
+        zofka.turnRight(0);
+        zofka.move(100);
+    }
+
+    private void posunSeNaDalsiDomek() {
+        zofka.penUp();
+        zofka.turnLeft(45);
+        zofka.move(50);
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnRight(180);
+        zofka.penDown();
     }
     public static void main(String[] args) {
         new HlavniProgram().start();
